e4fd115f5d5626e5ac717598876a805f5af1a7c0
Matheus Luz Rodrigues <86856853+Ma1heus@users.noreply.github.com>
2026-03-02 16:01:19 -0300

    Commit feito por mim

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,74 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string valueFromUser = Console.ReadLine();
+        int grade = int.Parse (valueFromUser);
+        string letter = "";
+        string sign = "";
+
+        if (grade > 100 || grade < 0){
+            Console.Write("Invalid Grade! Try again!");
+            return;
+        }
+        else {
+            if (grade >= 90)
+            {
+                letter = "A";
+            }
+            else if (grade >= 80)
+            {
+                letter = "B";
+            }
+            else if (grade >= 70)
+            {
+                letter = "C";
+            }
+            else if (grade >= 60)
+            {
+                letter = "D";
+            }
+            else
+            {
+                letter = "F";
+            }
+
+            if (grade % 10 >= 7)
+            {
+                if (grade > 90 || grade < 60)
+                {
+                    sign = "";
+                }
+                else
+                {
+                    sign = "+";
+                }
+            }
+            else if (grade % 10 < 3)
+            {
+                if (grade < 60)
+                {
+                    sign = "";
+                }
+                else
+                {
+                    sign = "-";
+                }
+            }
+            else
+            {
+                sign = "";
+            }
+
+            Console.WriteLine($"Your final grade is {letter}{sign}!");
+
+            if (grade >= 70){
+                Console.Write("Good Job! You are approved!");   
+            }
+            else
+            {
+                Console.Write("I'm sorry, but you failed. I know that you can do better, so keep trying!");
+            }
+        }
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string response = "yes";
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+            int tries = 0;
+
+            Console.WriteLine("I chose a number!");
+
+            while (true) 
+            {
+                Console.Write("What is your guess? ");
+                string valueFromUser = Console.ReadLine();
+                int guess = int.Parse(valueFromUser);
+                tries += 1;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"You tried {tries} times");
+                    Console.Write("Do you want to play again? ");
+                    response = Console.ReadLine();
+                    if (response == "no")
+                    {
+                        break;
+                    }
+                }
+            }
+            if (response == "no")
+            {
+                break;
+            }
+        } while (true);
     }
 }
