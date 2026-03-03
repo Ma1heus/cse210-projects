3e2f1785fc6cfdf4251cc63316ec583ca3a18a84
Matheus Luz Rodrigues <86856853+Ma1heus@users.noreply.github.com>
2026-03-03 15:57:21 -0300

    Fixing some issues

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,34 +1,64 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        DisplayWelcome();
-        string name = PromptUserName();
-        int number = PromptUserNumber();
-        DisplayResult(name, number);
-    }
-    static void DisplayWelcome()
-    {
-        Console.WriteLine("Welcome to the Program!");
-    }
-    static string PromptUserName()
-    {
-        Console.Write("Please enter your name: ");
-        string name = Console.ReadLine();
-        return name;
-    }
-    static int PromptUserNumber()
-    {
-        Console.Write("Please enter your favorite number: ");
-        int number = int.Parse(Console.ReadLine());
-        return number;
-    }
-    static void DisplayResult(string name,int number)
-    {
-        Console.Write($"{name}, the square of your number is {number*number}");
-    }
+        List<int> numberList = new List<int>();
+        Console.WriteLine("Enter a list of number, type 0 when finished: ");
+        int sum = 0;
+        int largestNumber = int.MinValue;
+        int smallestPositiveNumber = 0;
+        double average = 0;
 
-    
+        while (true)
+        {
+            Console.Write("Enter number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (smallestPositiveNumber == 0 && number > 0)
+            {
+                smallestPositiveNumber = number;
+            }
+            else if (number < smallestPositiveNumber && number > 0)
+            {
+                smallestPositiveNumber = number;
+            }
+
+            if (number != 0)
+            {
+                numberList.Add(number);
+                sum += number;
+            }
+
+            if (number > largestNumber)
+            {
+                largestNumber = number;
+            }
+
+            if (number == 0)
+            {
+                if (numberList.Count > 0)
+                {
+                    average = (double)sum / numberList.Count;
+                }
+
+                numberList.Sort();
+
+                Console.WriteLine($"The Sum is: {sum}");
+                Console.WriteLine($"The average is: {average:F2}");
+                Console.WriteLine($"The largest number is: {largestNumber}");
+                Console.WriteLine($"The smallest positive number is: {smallestPositiveNumber}");
+
+                Console.WriteLine("The sorted list is:");
+                foreach (int n in numberList)
+                {
+                    Console.WriteLine(n);
+                }
+
+                break;
+            }
+        }
+    }
 }
