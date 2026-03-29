caa9c03c96301dee0bdf83a4c1a3403f9126cb9a
Matheus Luz Rodrigues <86856853+Ma1heus@users.noreply.github.com>
2026-03-29 13:41:14 -0300

    Add extra videos and comments to channel
    Expand sample data in week04/YouTubeVideos/Program.cs: add an additional comment to the existing 'Pokemon' video, add multiple comments to 'Rocks', create two new videos ('One Piece' and 'Space Exploration') with accompanying comments, and add them to the channel. This change enriches the demo/test dataset used by the program.

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -13,11 +13,27 @@ class Program
         Video video1 = new Video("Pokemon", user1.GetName(), "5:00");
         video1.AddComment(new Comment("John", "Great video"));
         video1.AddComment(new Comment("Lucy", "Bad video"));
+        video1.AddComment(new Comment("Mark", "I like Pikachu"));
 
         Video video2 = new Video("Rocks", user2.GetName(), "20:00");
+        video2.AddComment(new Comment("Taylor", "This is absolutely a rock."));
+        video2.AddComment(new Comment("Brenda", "I don't believe that rocks exists"));
+        video2.AddComment(new Comment("Steve", "Very educational"));
+
+        Video video3 = new Video("One Piece", user1.GetName(), "15:00");
+        video3.AddComment(new Comment ("Taylor", "Luffy will be the pirates king!"));
+        video3.AddComment(new Comment("Mary", "I love Zoro!"));
+        video3.AddComment(new Comment("Jake", "Best anime ever"));
+
+        Video video4 = new Video("Space Exploration", user2.GetName(), "12:30");
+        video4.AddComment(new Comment("Alice", "Space is amazing!"));
+        video4.AddComment(new Comment("Bob", "I want to be an astronaut"));
+        video4.AddComment(new Comment("Clara", "Great explanation"));
 
         channel.AddVideo(video1);
         channel.AddVideo(video2);
+        channel.AddVideo(video3);
+        channel.AddVideo(video4);
 
         foreach (Video v in channel.GetVideos())
         {
